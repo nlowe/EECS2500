44e37c72f98902ce5557fc0664015b7877d3273c
Nathan Lowe <techwiz96@gmail.com>
2015-12-13 19:36:27 -0500

    Benchmark utility script should pass file path in quotes

--- WordCounter/benchmarks.sh
+++ WordCounter/benchmarks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mvn package -f ../pom.xml -pl WordCounter -am  -DskipTests=true
-find src/main/resources/ -type f -name '*.txt' | grep -v '[Ee]mpty' | xargs -L1 -I '{}' sh -c 'printf "\n\n=======\n\nTesting {}\n------\n"; java -jar target/WordCounter-1.0-SNAPSHOT-jar-with-dependencies.jar --file {}'
+find src/main/resources/ -type f -name '*.txt' | grep -v '[Ee]mpty' | xargs -L1 -I '{}' sh -c 'printf "\n\n=======\n\nTesting {}\n------\n"; java -jar target/WordCounter-1.0-SNAPSHOT-jar-with-dependencies.jar --file "{}"'
